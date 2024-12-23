9d6cde4d996b83a4c64b69fb0ff02d36a0cdf433
Matteo Campagnoli <matte.campa@yahoo.it>
2024-12-23 21:39:36 +0100

    forse adesso funziona: problema con i dividendi

--- DupirePrice.m
+++ DupirePrice.m
@@ -17,12 +17,16 @@ function price = DupirePrice(K,T)
 
     % Dupire Solver parameters - not sure about how to set K_min,K_max
     Lt = 100; Lh = 1000; K_min = 0.01; K_max = 3.5; scheme = 'cn';
-    [k,C] = solve_dupire(expiries,strikes,imp_volatilities,T,Lt,Lh,K_min,K_max,scheme);
+    [ k, C ] = solve_dupire(expiries,strikes,imp_volatilities,T,Lt,Lh,K_min,K_max,scheme);
     
-    fwd_at_expiry = forward(spot,expiries,disc_factors,q,T);
+    % normalized option price
+    fwd_at_expiry = forward(spot,expiries,r,q,T);
     norm_strike = K/fwd_at_expiry;
-
     norm_price = interp1(k,C,norm_strike);
+    %model_impl_vol_1 = blsimpv(1,norm_strike,0,T,norm_price);
+    
+    % option price
     disc_fact_at_expiry = discount(expiries,r,T);
     price = fwd_at_expiry * disc_fact_at_expiry * norm_price;
+    %model_impl_vol_2 = blsimpv(fwd_at_expiry,K,0,expiries,price/disc_fact_at_expiry);
 end
